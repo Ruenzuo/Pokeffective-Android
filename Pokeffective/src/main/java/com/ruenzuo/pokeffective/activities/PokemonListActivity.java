a4904369c91225ceb6c4d4aa389d97631b50ed00
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-04-16 22:52:46 +0200

    ActionBar implemented

--- Pokeffective/src/main/java/com/ruenzuo/pokeffective/activities/PokemonListActivity.java
+++ Pokeffective/src/main/java/com/ruenzuo/pokeffective/activities/PokemonListActivity.java
@@ -1,7 +1,10 @@
 package com.ruenzuo.pokeffective.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.ruenzuo.pokeffective.R;
 import com.ruenzuo.pokeffective.fragments.PokemonListFragment;
@@ -14,11 +17,26 @@ public class PokemonListActivity extends Activity implements PokemonListFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pokemon_list_activity);
-
     }
 
     @Override
     public void onCountrySelected(Pokemon pokemon) {
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- Pokeffective/src/main/java/com/ruenzuo/pokeffective/adapters/PokemonAdapter.java
+++ Pokeffective/src/main/java/com/ruenzuo/pokeffective/adapters/PokemonAdapter.java
@@ -5,8 +5,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.StateListDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,7 @@ import android.widget.TextView;
 
 import com.ruenzuo.pokeffective.R;
 import com.ruenzuo.pokeffective.models.Pokemon;
+import com.ruenzuo.pokeffective.utils.ColorUtils;
 
 /**
  * Created by ruenzuo on 16/04/14.
@@ -52,20 +52,35 @@ public class PokemonAdapter extends ArrayAdapter<Pokemon> {
         int resourceId = getContext().getResources().getIdentifier(resourceName, "drawable", getContext().getPackageName());
         Drawable drawable = getContext().getResources().getDrawable(resourceId);
         imgViewPokemonPicture.setImageDrawable(drawable);
-
         if (pokemon.getSecondType() != null) {
             int colors[] = new int[2];
             colors[0] = pokemon.getFirstType().toColor();
             colors[1] = pokemon.getSecondType().toColor();
-            GradientDrawable shape = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors);
-            shape.setCornerRadius(10 * Resources.getSystem().getDisplayMetrics().density);
-            convertView.setBackground(shape);
+            int darkerColors[] = new int[2];
+            darkerColors[0] = ColorUtils.darkerColor(colors[0]);
+            darkerColors[1] = ColorUtils.darkerColor(colors[1]);
+            GradientDrawable shapeUnselected = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors);
+            shapeUnselected.setCornerRadius(10 * Resources.getSystem().getDisplayMetrics().density);
+            GradientDrawable shapeSelected = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, darkerColors);
+            shapeSelected.setCornerRadius(10 * Resources.getSystem().getDisplayMetrics().density);
+            StateListDrawable states = new StateListDrawable();
+            states.addState(new int[] {android.R.attr.state_pressed}, shapeSelected);
+            states.addState(new int[] {android.R.attr.state_focused}, shapeSelected);
+            states.addState(new int[] { }, shapeUnselected);
+            convertView.setBackground(states);
         }
         else {
-            GradientDrawable shape = new GradientDrawable();
-            shape.setColor(pokemon.getFirstType().toColor());
-            shape.setCornerRadius(10 * Resources.getSystem().getDisplayMetrics().density);
-            convertView.setBackground(shape);
+            GradientDrawable shapeUnselected = new GradientDrawable();
+            shapeUnselected.setColor(pokemon.getFirstType().toColor());
+            shapeUnselected.setCornerRadius(10 * Resources.getSystem().getDisplayMetrics().density);
+            GradientDrawable shapeSelected = new GradientDrawable();
+            shapeSelected.setColor(ColorUtils.darkerColor(pokemon.getFirstType().toColor()));
+            shapeSelected.setCornerRadius(10 * Resources.getSystem().getDisplayMetrics().density);
+            StateListDrawable states = new StateListDrawable();
+            states.addState(new int[] {android.R.attr.state_pressed}, shapeSelected);
+            states.addState(new int[] {android.R.attr.state_focused}, shapeSelected);
+            states.addState(new int[] { }, shapeUnselected);
+            convertView.setBackground(states);
         }
         return convertView;
     }
