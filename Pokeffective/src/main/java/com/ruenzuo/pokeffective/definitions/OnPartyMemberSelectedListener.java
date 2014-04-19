a8bd0fd5801d87c19efecd99115d204461322dc3
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-04-19 22:59:50 +0200

    Party activity implemented

---
+++ Pokeffective/src/main/java/com/ruenzuo/pokeffective/definitions/OnPartyMemberSelectedListener.java
@@ -0,0 +1,12 @@
+package com.ruenzuo.pokeffective.definitions;
+
+import com.ruenzuo.pokeffective.models.Pokemon;
+
+/**
+ * Created by ruenzuo on 19/04/14.
+ */
+public interface OnPartyMemberSelectedListener {
+
+    public void onPartyMemberSelected(Pokemon member);
+
+}
---
+++ Pokeffective/src/main/java/com/ruenzuo/pokeffective/fragments/PartyListFragment.java
@@ -0,0 +1,82 @@
+package com.ruenzuo.pokeffective.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.nhaarman.listviewanimations.swinginadapters.prepared.SwingBottomInAnimationAdapter;
+import com.ruenzuo.pokeffective.R;
+import com.ruenzuo.pokeffective.adapters.PartyAdapter;
+import com.ruenzuo.pokeffective.definitions.OnPartyMemberSelectedListener;
+import com.ruenzuo.pokeffective.models.Pokemon;
+import com.ruenzuo.pokeffective.tasks.BoxTask;
+import com.telly.groundy.Groundy;
+import com.telly.groundy.annotations.OnSuccess;
+import com.telly.groundy.annotations.Param;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ruenzuo on 19/04/14.
+ */
+public class PartyListFragment extends ListFragment {
+
+    private OnPartyMemberSelectedListener listener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (activity instanceof OnPartyMemberSelectedListener) {
+            listener = (OnPartyMemberSelectedListener)activity;
+        } else {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnPartyMemberSelectedListener");
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        PartyAdapter adapter = new PartyAdapter(getActivity(), R.layout.member_row, new ArrayList<Pokemon>());
+        SwingBottomInAnimationAdapter swingRightInAnimationAdapter = new SwingBottomInAnimationAdapter(adapter);
+        swingRightInAnimationAdapter.setAbsListView(getListView());
+        setListAdapter(swingRightInAnimationAdapter);
+        Groundy.create(BoxTask.class)
+                .callback(this)
+                .queueUsing(getActivity());
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Pokemon pokemon = (Pokemon)getListAdapter().getItem(position);
+        listener.onPartyMemberSelected(pokemon);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.party_list_fragment, container, false);
+        return view;
+    }
+
+    @OnSuccess(BoxTask.class)
+    public void onSuccess(@Param("Box") ArrayList<Pokemon> box) {
+        ArrayList<Pokemon> filteredBox = new ArrayList<Pokemon>();
+        for(Pokemon pokemon : box) {
+            if (pokemon.moves().size() >= 4) {
+                filteredBox.add(pokemon);
+            }
+        }
+        SwingBottomInAnimationAdapter listAdapter = (SwingBottomInAnimationAdapter)getListAdapter();
+        listAdapter.setShouldAnimateFromPosition(0);
+        PartyAdapter adapter = (PartyAdapter)listAdapter.getDecoratedBaseAdapter();
+        adapter.addAll(filteredBox);
+        adapter.notifyDataSetChanged();
+    }
+
+}
