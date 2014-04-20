6b05d0b787c3973b8bf67a8af98ad1131c8b3e5d
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-04-20 13:58:54 +0200

    Clearing selection

---
+++ Pokeffective/src/main/java/com/ruenzuo/pokeffective/definitions/OnPartySelectionClearedListener.java
@@ -0,0 +1,10 @@
+package com.ruenzuo.pokeffective.definitions;
+
+/**
+ * Created by ruenzuo on 20/04/14.
+ */
+public interface OnPartySelectionClearedListener {
+
+    public void onPartySelectionCleared();
+
+}
